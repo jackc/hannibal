8c0ac352e8051344a007f7e5f24382cdc3a60f02
Jack Christensen <jack@jackchristensen.com>
2020-06-13 08:10:18 -0500

    Initial commit

---
+++ cmd/serve.go
@@ -0,0 +1,25 @@
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+// serveCmd represents the serve command
+var serveCmd = &cobra.Command{
+	Use:   "serve",
+	Short: "Start web server",
+	Run: func(cmd *cobra.Command, args []string) {
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(serveCmd)
+
+	serveCmd.Flags().StringP("http-service-address", "a", "127.0.0.1:3000", "HTTP service address")
+	viper.BindPFlag("http_service_address", serveCmd.Flags().Lookup("http-service-address"))
+
+	serveCmd.Flags().StringP("database-url", "d", "", "Database URL or DSN")
+	viper.BindPFlag("database_url", serveCmd.Flags().Lookup("database-url"))
+}
