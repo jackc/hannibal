9f622edc9b2602d3a96914f8788d462d1d6d10a4
Jack Christensen <jack@jackchristensen.com>
2020-08-01 13:23:26 -0500

    Deploy cycles through next, current, and previous directories.

--- server/system_handler.go
+++ server/system_handler.go
@@ -4,12 +4,14 @@ import (
 	"context"
 	"encoding/hex"
 	"errors"
-	"fmt"
 	"net/http"
+	"os"
+	"path/filepath"
 	"strings"
 	"sync"
 
 	"github.com/go-chi/chi"
+	"github.com/jackc/hannibal/current"
 	"github.com/jackc/hannibal/deploy"
 	"github.com/jackc/hannibal/system"
 	"github.com/jackc/pgx/v4"
@@ -38,8 +40,6 @@ func (sh *SystemHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	sh.reloadMutex.RLock()
 	defer sh.reloadMutex.RUnlock()
 
-	fmt.Fprintln(w, "it got here")
-
 	sh.router.ServeHTTP(w, req)
 }
 
@@ -63,6 +63,7 @@ func (sh *SystemHandler) deploy(w http.ResponseWriter, req *http.Request) {
 		if errors.Is(err, pgx.ErrNoRows) {
 			http.Error(w, "Authorization header contains incorrect API key", http.StatusForbidden)
 		} else {
+			current.Logger(ctx).Error().Caller().Err(err).Send()
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
 		return
@@ -73,6 +74,7 @@ func (sh *SystemHandler) deploy(w http.ResponseWriter, req *http.Request) {
 		if errors.Is(err, http.ErrMissingFile) {
 			http.Error(w, "Missing pkg file upload", http.StatusBadRequest)
 		} else {
+			current.Logger(ctx).Error().Caller().Err(err).Send()
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
 		return
@@ -81,23 +83,70 @@ func (sh *SystemHandler) deploy(w http.ResponseWriter, req *http.Request) {
 
 	signature, err := hex.DecodeString(req.FormValue("signature"))
 	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
 	publicKeys, err := system.GetDeployPublicKeysForUserID(ctx, userID)
 	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
 		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 
-	err = deploy.Unpack(pkg, signature, sh.appPath, publicKeys)
+	nextPath := filepath.Join(sh.appPath, "next")
+	currentPath := filepath.Join(sh.appPath, "current")
+	previousPath := filepath.Join(sh.appPath, "previous")
+
+	err = os.RemoveAll(nextPath)
+	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = os.Mkdir(nextPath, 0777)
+	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = deploy.Unpack(pkg, signature, nextPath, publicKeys)
 	if err != nil {
 		if errors.Is(err, deploy.ErrInvalidSignature) {
 			http.Error(w, "Invalid signature", http.StatusBadRequest)
 		} else {
+			current.Logger(ctx).Error().Caller().Err(err).Send()
 			http.Error(w, "Internal server error", http.StatusInternalServerError)
 		}
+		return
+	}
+
+	// TODO - deploy database code -- then once successfully deployed
+
+	err = os.RemoveAll(previousPath)
+	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
+		return
+	}
+
+	err = os.Rename(currentPath, previousPath)
+	if err != nil {
+		// On the first deploy currentPath will not exist.
+		if !errors.Is(err, os.ErrNotExist) {
+			current.Logger(ctx).Error().Caller().Err(err).Send()
+			http.Error(w, "Internal server error", http.StatusInternalServerError)
+			return
+		}
+	}
+
+	err = os.Rename(nextPath, currentPath)
+	if err != nil {
+		current.Logger(ctx).Error().Caller().Err(err).Send()
+		http.Error(w, "Internal server error", http.StatusInternalServerError)
 		return
 	}
 }
